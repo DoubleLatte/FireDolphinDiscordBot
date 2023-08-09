98536f35a4de9a99784baeaeb9bd2ab64e25e675
DoubleLatte <85533915+DoubleLatte@users.noreply.github.com>
2023-08-09 20:40:08 +0900

    Update suport.py

--- command/suport.py
+++ command/suport.py
@@ -4,15 +4,14 @@ from discord import Interaction
 from discord import Object
 import discord
 import datetime
-now = datetime.datetime.now()
-time = now.strftime('%Y-%m-%d %H:%M:%S')
+
 
 class out(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
     @app_commands.command(name="지원",description="돌핀 최강 소방청 지원하기")  
-    async def outs(self, interaction: Interaction) -> None:
+    async def outs(self, interaction: Interaction,닉네임:str,나이:str,지원동기:str) -> None:
         user_id = str (interaction.user.id)
         user_name = interaction.user.name
         user_nick = interaction.user.nick 
@@ -21,12 +20,16 @@ class out(commands.Cog):
         time = now.strftime('%Y-%m-%d %H:%M:%S')
 
 
-        if "1094245109199028280" in user_role or "1132891083148374067" in user_role:
+        if "1094245109199028280" in user_role or "1094973339514179666" in user_role:
+            now = datetime.datetime.now()
             time = now.strftime('%Y-%m-%d %H:%M:%S')
-            embed=discord.Embed(title=":fire_engine: **"+user_nick +"** 님의 퇴근시간", color=0xb1daf5)
+            embed=discord.Embed(title=user_nick +"** 님의 소방청 지원서", color=0xb1daf5)
             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1133079804250431540/1135882626201288755/-_-009.png")
             embed.set_author(name="")
-            embed.add_field(name="**"+time+"**",value="", inline=False)
+            embed.add_field(name="닉네임: "+ 닉네임,value="", inline=False)
+            embed.add_field(name="나이: "+ 나이,value="", inline=False)
+            embed.add_field(name="지원동기: "+ 지원동기,value="", inline=False)
+            embed.set_footer(text=time)
             await interaction.response.send_message("<@!"+user_id+">",embed=embed)
             print(user_name + ' 퇴근 도장 사용')
 
