71d2e277a619f6b63dd991d0acec42194f8c7c94
DoubleLatte <85533915+DoubleLatte@users.noreply.github.com>
2023-08-15 23:09:01 +0900

    Update sell.py

--- command/sell.py
+++ command/sell.py
@@ -10,8 +10,8 @@ class sell(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(name="판매장부",description="판매 장부 적기 사잔은 따로 추가")  
-    async def sell(self, interaction: Interaction,구매자:str,수량:str,금액:str,직업:str) -> None:
+    @app_commands.command(name="판매장부",description="판매 장부 적기 사진은 패더클 업로드로 추가")  
+    async def sell(self, interaction: Interaction,구매자:str,이미지:str,수량:str,금액:str,직업:str) -> None:
         user_id = str (interaction.user.id)
         user_name = interaction.user.name
         user_nick = interaction.user.nick
@@ -20,7 +20,7 @@ class sell(commands.Cog):
         if "1094245109199028280" in user_role or "1132891083148374067" in user_role:        
             time = now.strftime('%Y-%m-%d %H:%M:%S')
             embed=discord.Embed(title="**"+user_nick +"** 님의 판매 내역", color=0x1b67e0)
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1133079804250431540/1135882626201288755/-_-009.png")
+            embed.set_thumbnail(url=이미지)
             embed.set_author(name="")
             embed.add_field(name="**구매자: **"+ 구매자 ,value="", inline=False)
             embed.add_field(name="**판매 수량: **"+ 수량,value="", inline=False)
